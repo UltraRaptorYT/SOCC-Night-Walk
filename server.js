e189c24fc81b2e853bb533230a64aa04070b23d8
Soh Hong Yu <sohhongyu@gmail.com>
2023-03-15 20:46:41 +0800

    dotenv

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-/*
-Name: Soh Hong Yu
-Class: DAAA/FT/1B/01
-Admin No.: P2100775
-*/
-
+require("dotenv").config();
 const express = require("express");
 const serveStatic = require("serve-static");
 
@@ -15,8 +10,9 @@ app.use(serveStatic(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html", { root: __dirname + "/public" });
-})
+});
 
 app.listen(port, function () {
+  console.log(process.env.SUPABASE_KEY);
   console.log(`Server hosted at http://localhost:${port}`);
 });
