fbb0c7eebbf381285513605daed574e7b6a51dda
Soh Hong Yu <sohhongyu@gmail.com>
2023-03-15 13:55:38 +0800

    Update UI

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ var app = express();
 
 app.use(serveStatic(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile("/index.html", { root: __dirname + "/public" });
+})
+
 app.listen(port, function () {
   console.log(`Server hosted at http://localhost:${port}`);
 });
