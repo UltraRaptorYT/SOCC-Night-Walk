1637ad1700cdd122eb58c617caf4b07453f6f0e7
Soh Hong Yu <sohhongyu@gmail.com>
2023-03-15 11:44:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ Admin No.: P2100775
 const express = require("express");
 const serveStatic = require("serve-static");
 
-var hostname = "localhost";
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(serveStatic(__dirname + "/public"));
 
-app.listen(port, hostname, function () {
-  console.log(`Server hosted at http://${hostname}:${port}`);
+app.listen(port, function () {
+  console.log(`Server hosted at http://localhost:${port}`);
 });
